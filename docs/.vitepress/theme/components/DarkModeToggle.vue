3bcecf2b3b3f4316f4c8e8a3a7f105e87f58b9cc
xbrooke <554719247@qq.com>
2025-07-04 12:52:43 +0800

    Create DarkModeToggle.vue

---
+++ docs/.vitepress/theme/components/DarkModeToggle.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="toggle" @click="toggleDark()">
+    <span class="dark-icon">🌙</span>
+    <span class="light-icon">☀️</span>
+  </button>
+</template>
+
+<script setup>
+import { useData } from 'vitepress'
+const { isDark } = useData()
+
+const toggleDark = () => {
+  isDark.value = !isDark.value
+}
+</script>
+
+<style scoped>
+.toggle {
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 8px;
+  font-size: 1.2em;
+}
+
+.light-icon {
+  display: none;
+}
+
+.dark .light-icon {
+  display: inline;
+}
+
+.dark .dark-icon {
+  display: none;
+}
+</style>
